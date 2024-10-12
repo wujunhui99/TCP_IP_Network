88256d1a70c22ca8a00b016288bf7c0647af1149
wujunhui <344686925@qq.com>
2024-10-12 21:17:19 +0800

    mac first commit

---
+++ ch01/low_open.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+void error_handling(char *message);
+
+int main()
+{
+    int fd;
+    char buf[] = "Let's go!\n";
+    // O_CREAT | O_WRONLY | O_TRUNC 是文件打开模式，将创建新文件，并且只能写。如存在 data.txt 文件，则清空文件中的全部数据。
+    fd = open("data.txt", O_CREAT | O_WRONLY | O_TRUNC);
+    if (fd == -1)
+        error_handling("open() error!");
+    // 向对应 fd 中保存的文件描述符的文件传输 buf 中保存的数据。
+    int fd2 = open("data2.txt", O_CREAT | O_WRONLY | O_TRUNC);
+    if (fd == -1)
+        error_handling("open() error!");
+    printf("file descriptor: %d \n", fd);
+    printf("file descriptor2: %d \n", fd2);
+    if (write(fd, buf, sizeof(buf)) == -1)
+        error_handling("write() error!");
+    close(fd);
+     if (write(fd2, buf, sizeof(buf)) == -1)
+        error_handling("write() error!");
+    
+    close(fd);
+    close(fd2);
+    
+    return 0;
+}
+
+void error_handling(char *message)
+{
+    fputs(message, stderr);
+    fputc('\n', stderr);
+    exit(1);
+}
---
+++ ch04/test.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+#define BUF_SIZE 1024
+int main(int argc, char *argv[])
+{
+   
+
+  
+        while(1){
+            char message[BUF_SIZE];
+        fputs("Input message(Q to quit): ", stdout);
+        fgets(message, BUF_SIZE, stdin);
+        }
+        
+
+
+        return 0;
+}
+
